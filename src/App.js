0c229c3eb5561dbe8133b15fbcb9d95120fe696e
Gyumong <rlaalsrb3559@naver.com>
2020-03-12 22:07:56 +0900

    (ref:Dom에 이름달기)는 'Dom을 꼭 직접적으로 건드려야 할때' 사용한다

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
-import EventPractice from './EventPractice';
-/*
-state는 컴포넌트 내부에서 바뀔 수 있는 값을 의미
-바뀔때마다 컴포넌트는 re렌더링이 되고 바꿀때 마다 setState을 사용해야됨
-안쓰면 re렌더링 하지않아서 원하는 값이 나타나지 않음
-props는 부모가 전달해주는 값 state는 자기자신이 갖고 있는 값
-props는 읽기전용 state는 변경할수도있음
-*/
-const App = () => {
-  return <EventPractice / >;
-};
+import ScrollBox from './ScrollBox';
+
+class App extends Component{
+  render(){
+    return (
+      <div>
+      <ScrollBox ref={(ref)=> this.ScrollBox=ref}/>
+      <button onClick={()=> this.ScrollBox.scrollToBottom()}>
+      맨밑으로</button>
+      </div>
+    );
+  }
+}
 
 export default App;
